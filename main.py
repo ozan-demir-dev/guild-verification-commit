13d6758164d06efe76c3885c44105949c483c60d
ozan demir <ha7484182@gmail.com>
2025-10-30 21:44:02 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T18:43:42.813Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-30T18:43:59.914Z
+Commit: Patch API endpoint error handling
